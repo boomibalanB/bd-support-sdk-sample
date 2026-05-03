cace06fce7a7b6e84ddf35da05bb5e277ebd2156
BoldDesk <147583808+jalagandeswaransf4225@users.noreply.github.com>
2026-05-03 13:34:57 +0530

    changes

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "BoldDeskSupportSDK",
-            type: .static,
             targets: ["BoldDeskSupportSDK"]
         )
     ],
